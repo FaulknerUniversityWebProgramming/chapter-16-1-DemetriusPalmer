ee2c4fef19a92c5b0d38a25aaaf1398637c80c6e
DemetriusPalmer <demetrius.palmer@faulkner.edu>
2018-12-03 23:45:26 -0600

    Step 3

--- chapter16-project1.php
+++ chapter16-project1.php
@@ -1,6 +1,13 @@
 <?php
+$expiryTime= time()+60+60*24;
 
+$name= "Theme";
+$name2= "Philosopher";
 
+setcookie($name, $_POST['philosopher'], $expiryTime, null, setcookie(), null, true);
+setcookie($name2, $_POST['philosopher'], 0, null, setcookie(), null, true);
+
+header("Location: chapter-project1.php");
 
 ?>
 <!DOCTYPE html>
